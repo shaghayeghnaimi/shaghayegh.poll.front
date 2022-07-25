a0a8433fbd779321b8e1133c4932bc77171fa784
Shaghayeghnaimi <shaghayegh.naimi96@gmail.com>
2022-07-25 21:20:32 +0430

    Appbar is became responsive

--- src/App.js
+++ src/App.js
@@ -1,65 +1,246 @@
+import * as React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import TableFooter from "@mui/material/TableFooter";
+import "./App.css";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import LeaderboardIcon from "@mui/icons-material/Leaderboard";
 import Home from "./Home";
 import PollList from "./PollList";
 import PollPage from "./PollPage";
+import PollCreate from "./PollCreate";
+import Login from "./Login";
 
-import "./App.css";
-import main from "./main.png";
 
-export default function App() {
+
+const App = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <Router>
-      <div className="header">
-        <div className="logo">
-          <img className="main_image" src={main} alt="logo" />
-          <Link to="/" className="title">
-            Easy Poll
-          </Link>
-        </div>
-        <div className="content">
-          <Link to="/PollList" className="content-name">
-            Manage Polls
-          </Link>
-          <Link to="/PollPage" className="content-name">
-            Results
-          </Link>
-          <Link to="/PollList" className="content-name">
-            Poll List
-          </Link>
-        </div>
-        <div className="button-login">
-          <Button variant="contained">Log in</Button>
-        </div>
-        <div className="button-create">
-          <Button variant="outlined">Create +</Button>
-        </div>
-      </div>
+    <AppBar position="static" style={{ backgroundColor: "white" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <LeaderboardIcon
+            style={{ color: "black" }}
+            sx={{ fontSize: 40, display: { xs: "none", md: "flex" }, mr: 1 }}
+          />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <Link to="/" className="title">
+              Easy Poll
+            </Link>
+          </Typography>
 
-      <Routes>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              style={{ color: "black" }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem onClick={handleCloseNavMenu}>
+                <div
+                  style={{ display: "block" }}
+                  textAlign="center"
+                  color="black"
+                >
+                  <Link to="/PollList" className="content-name">
+                    Manage Polls
+                  </Link>
+                </div>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <div
+                  style={{ display: "block" }}
+                  textAlign="center"
+                  color="black"
+                >
+                  <Link to="/PollPage" className="content-name">
+                    Results
+                  </Link>
+                </div>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <div
+                  style={{ display: "block" }}
+                  textAlign="center"
+                  color="black"
+                >
+                  <Link to="/PollList" className="content-name">
+                    Poll List
+                  </Link>
+                </div>
+              </MenuItem>
+
+              <hr />
+              <Link to="/Login">
+                <Button variant="contained" sx={{ mt: 1, ml: "24px", mb: 2 }}>
+                  Log in
+                </Button>
+              </Link>
+            </Menu>
+          </Box>
+
+          <LeaderboardIcon
+            style={{ color: "black" }}
+            sx={{ fontSize: 24, display: { xs: "flex", md: "none" }, mt: 1 }}
+          />
+
+          <Typography
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontSize: "24px",
+              fontFamily: "monospace",
+              fontWeight: 700,
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <Link to="/" className="title">
+              Easy Poll
+            </Link>
+          </Typography>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Create a new poll">
+              <Link to="/PollCreate">
+                <Button
+                  variant="outlined"
+                  sx={{
+                    display: { xs: "flex", md: "none" },
+                    pr: "2px",
+                    pl: "2px",
+                  }}
+                >
+                  +
+                </Button>
+              </Link>
+            </Tooltip>
+          </Box>
+
+          <Box
+            sx={{ flexGrow: 1, display: { xs: "none", md: "inline-block" } }}
+          >
+            <Button sx={{ ml: 4 }} onClick={handleCloseNavMenu}>
+              <Link to="/PollList" className="content-name">
+                Manage Polls
+              </Link>
+            </Button>
+            <Button sx={{ ml: 2 }} onClick={handleCloseNavMenu}>
+              <Link to="/PollPage" className="content-name">
+                Results
+              </Link>
+            </Button>
+            <Button sx={{ ml: 2 }} onClick={handleCloseNavMenu}>
+              <Link to="/PollList" className="content-name">
+                Poll List
+              </Link>
+            </Button>
+            <Link to="/Login">
+              <Button variant="contained" sx={{ ml: 4 }}>
+                Log in
+              </Button>
+            </Link>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Create a new poll">
+              <Link to="/PollCreate">
+                <Button
+                  variant="outlined"
+                  sx={{
+                    display: { xs: "none", md: "inline-block" },
+                    paddingRight: "16px",
+                  }}
+                >
+                  Create +
+                </Button>
+              </Link>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/PollList" element={<PollList />} />
         <Route path="/PollPage" element={<PollPage />} />
+        <Route path="/PollCreate" element={<PollCreate />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
 
       <div className="footer">
+   
         <div className="icon-connection">
-          <InstagramIcon
-            sx={{ color: "purple", marginRight: "16px", marginTop: "24px" }}
-          />
-          <TelegramIcon sx={{ color: "blue", marginRight: "16px" }} />
-          <WhatsAppIcon sx={{ color: "green", marginRight: "16px" }} />
-          <YouTubeIcon sx={{ color: "red", marginRight: "16px" }} />
+          <InstagramIcon sx={{ marginRight: "16px" }} />
+          <TelegramIcon sx={{ marginRight: "16px" }} />
+          <WhatsAppIcon sx={{ marginRight: "16px" }} />
+          <YouTubeIcon sx={{ marginRight: "16px" }} />
         </div>
         <div className="information">
           <div className="us">
             <p>َAbout us</p>
             <p>Contact Us</p>
+            <p>Help center</p>
           </div>
           <div className="address">
             <b>Address:</b>
@@ -74,4 +255,5 @@ export default function App() {
       </div>
     </Router>
   );
-}
+};
+export default App;
