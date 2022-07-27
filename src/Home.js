471dd3248f2aa560caac48d94a3c73be533ea3b4
Shaghayeghnaimi <shaghayegh.naimi96@gmail.com>
2022-07-27 13:04:38 +0430

    Main div for main explonation of landing  are built

--- src/Home.js
+++ src/Home.js
@@ -1,137 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
 
-import Rating from "@mui/material/Rating";
+import Button from "@mui/material/Button";
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 
 export default function Home() {
   return (
     <div className="main">
       <div className="container">
-      <div className="product">
-          <div>
-            <Link to="/Product2">
-           
-            </Link>
-          </div>
-          <div>
-            <h2>Soimoi White Cotton Voile</h2>
-            <Rating
-              name="half-rating-read"
-              defaultValue={3.5}
-              precision={0.5}
-              readOnly
-            />
-            <p>
-              we create unique designs with digitally printed fabrics which can
-              help you to create a product we could not have imagined and that's
-              the power of individual thinking by Super minds.
-            </p>
-          </div>
-        </div> <div className="product">
-          <div>
-            <Link to="/Product2">
-           
-            </Link>
-          </div>
-          <div>
-            <h2>Soimoi White Cotton Voile</h2>
-            <Rating
-              name="half-rating-read"
-              defaultValue={3.5}
-              precision={0.5}
-              readOnly
-            />
-            <p>
-              we create unique designs with digitally printed fabrics which can
-              help you to create a product we could not have imagined and that's
-              the power of individual thinking by Super minds.
-            </p>
-          </div>
-        </div> <div className="product">
-          <div>
-            <Link to="/Product2">
-           
-            </Link>
-          </div>
-          <div>
-            <h2>Soimoi White Cotton Voile</h2>
-            <Rating
-              name="half-rating-read"
-              defaultValue={3.5}
-              precision={0.5}
-              readOnly
-            />
-            <p>
-              we create unique designs with digitally printed fabrics which can
-              help you to create a product we could not have imagined and that's
-              the power of individual thinking by Super minds.
-            </p>
-          </div>
+        <div className="main">
+<p className="explain">Make poll,</p>
+<p className="explain">Just by 3 steps</p>
+<p className="explain1">Do it easy and fast</p>
+<Button variant="contained" endIcon={<ArrowCircleRightOutlinedIcon />}>
+        Start Free Trial
+      </Button>
         </div>
-        <div className="product">
-          <div>
-            <Link to="/Product1">
-          
-            </Link>
-          </div>
-          <div>
-            <h2>Soimoi Rose Floral Printed</h2>
+        <div className="main">
 
-            <Rating
-              name="half-rating-read"
-              defaultValue={5}
-              precision={0.5}
-              readOnly
-            />
-            <p>
-              It is always a good idea to empower the world with your abilities
-              and let everybody or everyone use their creativity for the best
-              outcome. that's the power of individual thinking by Super minds.
-            </p>
-          </div>
+
         </div>
-        <div className="product">
-          <div>
-            <Link to="/Product2">
-           
-            </Link>
-          </div>
-          <div>
-            <h2>Soimoi White Cotton Voile</h2>
-            <Rating
-              name="half-rating-read"
-              defaultValue={3.5}
-              precision={0.5}
-              readOnly
-            />
-            <p>
-              we create unique designs with digitally printed fabrics which can
-              help you to create a product we could not have imagined and that's
-              the power of individual thinking by Super minds.
-            </p>
-          </div>
-        </div>
-        <div className="product">
-          <div>
-            <Link to="/Product3">
-           
-            </Link>
-          </div>
-          <div>
-            <h2>Soimoi White Cotton Cambric</h2>
-            <Rating
-              name="half-rating-read"
-              defaultValue={1.5}
-              precision={0.5}
-              readOnly
-            />
-            <p>
-              It is always a good idea to empower the world with your abilities
-              and let everybody or everyone use their creativity for the best
-              outcome.
-            </p>
-          </div>
-        </div>
+    
+        
       </div>
     </div>
   );
