0cdaf2ba4b7c61ee5808c1c61e118ae70a499fe9
Shaghayeghnaimi <shaghayegh.naimi96@gmail.com>
2022-08-14 23:00:28 +0430

    it is completed and show all polls

--- src/PollList.js
+++ src/PollList.js
@@ -1,10 +1,62 @@
-export default function PollList() {
+import React, { useState, useEffect } from "react";
+import "./PollList.css";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Link } from "react-router-dom";
 
-  
-    return (
-      <div className="container">
-       
-      </div>
-    );
-  }
-  
+function PollList() {
+  const [data, getData] = useState([]);
+  const URL = "http://localhost:3004/poll/";
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch(URL)
+      .then((res) => res.json())
+
+      .then((response) => {
+        console.log(response);
+        getData(response);
+      });
+  };
+
+  return (
+    <>
+      <h1>Poll List</h1>
+      <tbody>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Link</th>
+          <th>Nunber of Participent</th>
+          <th></th>
+        </tr>
+        {data.map((item, i) => (
+          <tr key={i}>
+            <td>{item.title}</td>
+            <td>{item.description}</td>
+            <td>{item.link}</td>
+            <td>{item.id}</td>
+
+            <td>
+              {" "}
+              <IconButton aria-label="delete" color="primary">
+                <DeleteIcon />
+              </IconButton>
+              <Link to="/PollManage">
+                <IconButton aria-label="Edit" color="primary">
+                  <EditIcon />
+                </IconButton>
+              </Link>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </>
+  );
+}
+
+export default PollList;
