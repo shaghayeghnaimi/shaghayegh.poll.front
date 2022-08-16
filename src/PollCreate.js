14ea61ae1da9ec79200105c18821d4618ec809a1
Shaghayeghnaimi <shaghayegh.naimi96@gmail.com>
2022-08-17 00:03:34 +0430

    add items

--- src/PollCreate.js
+++ src/PollCreate.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import IconButton from "@mui/material/IconButton";
- import DeleteIcon from "@mui/icons-material/Delete";
+import DeleteIcon from "@mui/icons-material/Delete";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
@@ -14,6 +14,8 @@ export default function PollCreate() {
   const [title, setTitle] = React.useState("");
   const [description, setDesc] = React.useState("");
   const [newItems, setNewItems] = React.useState([]);
+  const [titleErr, setTitleErr] = React.useState("");
+  const [descErr, setDescErr] = React.useState("");
 
   const addOption = () => {
     if (item === "") {
@@ -31,52 +33,73 @@ export default function PollCreate() {
       options.filter((option) => currentOption !== option)
     );
   };
-  const dataChoice = {
-    item:item
-  }
-// const dataChoice ={
-//   item: item
-// }
 
-// const createChoice = () =>{
-//     axios
-//       .post("http://localhost:3004/PollChoice", dataChoice)
-//       .then((res) => {
-//         const id = res.choice;
-     
-//       })
-//       .catch((error) => console.log("error :>> ", error));
-
- 
-// }
-  const dataPoll = {
+  const dataPoll = JSON.stringify({
     title: title,
     description: description,
-    link: "jnkbjk.bkjb.kjb"
-  };
+    link: "uniqueLink",
+  });
 
   const createUniqueLink = (id) => {
     return `http://localhost:3004/poll/${id}`;
   };
 
   const createPoll = () => {
-    const token= localStorage.getItem("token");
-if(token){
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-}
-    axios
-      .post("http://localhost:3004/poll/", dataPoll)
+    setTitleErr("");
+    setDescErr("");
+    if (title === "" || description === "") {
+      setTitleErr("Title is required.");
+      setDescErr("Description is required.");
+      return;
+    }
+
+    const token = localStorage.getItem("token");
+    const config = {
+      method: "post",
+      url: "http://localhost:3004/poll/",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+      data: dataPoll,
+    };
+
+    axios(config)
       .then((res) => {
-        const id = res.id;
-        const uniqueLink = createUniqueLink(id);
+        const id = res.data.insertId;
+        console.log("id :>> ", id);
+        createItems(id);
+        const uniqueLink = createUniqueLink(id + 1);
         setUniqueLink(uniqueLink);
-      
       })
       .catch((error) => console.log("error :>> ", error));
 
     setDesc("");
     setTitle("");
     setItem("");
+  };
+  const createItems = (insertId) => {
+    const token = localStorage.getItem("token");
+    const myItems = newItems.map((item) => {
+      return { poll_id: insertId, item: item };
+    });
+
+    const config = {
+      method: "post",
+      url: "http://localhost:3004/choice/",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
+      },
+      data: myItems,
+    };
+    axios(config)
+      .then((response) => {
+        console.log("response :>> ", response);
+      })
+      .catch((Err) => {
+        console.log("Err :>> ", Err);
+      });
   };
 
   return (
@@ -85,7 +108,11 @@ if(token){
         <div className="create-container">
           <h1 className="create-h1">New Poll</h1>
           <TextField
-            style={{ width: "100%", marginTop: "40px", marginBottom: "32px" }}
+            style={{
+              width: "100%",
+              marginTop: "40px",
+              marginBottom: titleErr ? "0px" : "32px",
+            }}
             required
             value={title}
             onChange={(e) => setTitle(e.target.value)}
@@ -93,8 +120,11 @@ if(token){
             label="Title"
             variant="outlined"
           />
+          <div style={{ color: "red", marginBottom: "32px" }}>
+            <span> {title === "" ? titleErr : ""}</span>
+          </div>
           <TextField
-            style={{ width: "100%", marginBottom: "32px" }}
+            style={{ width: "100%", marginBottom: descErr ? "0px" : "32px" }}
             required
             value={description}
             onChange={(e) => setDesc(e.target.value)}
@@ -102,6 +132,9 @@ if(token){
             label="Description"
             variant="outlined"
           />
+          <div style={{ color: "red", marginBottom: "32px" }}>
+            <span> {description === "" ? descErr : ""}</span>
+          </div>
           <TextField
             style={{ width: "100%", marginBottom: "16px" }}
             value={item}
@@ -118,30 +151,28 @@ if(token){
           </IconButton>
         </div>
         <div className="create-result">
-        <div>
-          {newItems.map((option) => {
-            return (
-              <div key={option}>
-                {option}
+          <div>
+            {newItems.map((option) => {
+              return (
+                <div key={option}>
+                  {option}
 
-                <IconButton
-                  onClick={() => deleteOption(option)}
-                  style={{ color: "red" }}
-                  edge="end"
-                  aria-label="delete"
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </div>
-            );
-          })}
-
+                  <IconButton
+                    onClick={() => deleteOption(option)}
+                    style={{ color: "red" }}
+                    edge="end"
+                    aria-label="delete"
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
         <div>
           <div className="create-button">
             <Button
-             
               variant="contained"
               onClick={createPoll}
               startIcon={<AddIcon />}
@@ -151,7 +182,6 @@ if(token){
           </div>
         </div>
       </div>
-     
     </div>
   );
 }
